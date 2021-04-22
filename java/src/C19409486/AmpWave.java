86a18ebf3feb71f3908283b8fc681306df1de0a7
Alex Bang <Alex.ZBP2456@GMAIL.COM>
2021-04-22 18:22:32 +0100

    Finished Amp waves

--- java/src/C19409486/AmpWave.java
+++ java/src/C19409486/AmpWave.java
@@ -6,7 +6,6 @@ import processing.core.PApplet;
 public class AmpWave extends MusicObject {
 
     
-
     public AmpWave(AlexVisual av){
         this._av = av;
     }
@@ -22,84 +21,16 @@ public class AmpWave extends MusicObject {
         for(int i = 0; i < _av.getAudioBuffer().size()-1; i++){
             float temp[] = new float[_av.getAudioBuffer().size()];
 
-            // lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.1f);
-            //setLerpedBuffer() = lerp(getLerpedBuffer().get(i),  _av.getAudioBuffer().get(i), 0.1f);
-
             _av.stroke(PApplet.map(i, 0, _av.getAudioBuffer().size(), 0, 255), 255, 255);
           
-
-            
-            //_av.point(i* 3, _av.getCy() + (_av.getAudioBuffer().get(i)* _av.getCy()), 0);
-            //_av.line(i * 3 , _av.getCy() + (_av.getAudioBuffer().get(i)* _av.getCy()), prevX, prevY);
-
             temp = generateLerp(_av.getAudioBuffer(), _av);
 
-          
-            //_av.line(100, 100, 0, 200, 100, 0);
-           // _av.line(i* 3,_av.height/2 + (temp[i]* _av.height/2), 0, i* 3, _av.height/2+ (temp[i] *_av.height/2), 0);
             _av.strokeWeight(1);
             _av.point(i* 3,_av.height/2 + (_av.getAudioBuffer().get(i)* _av.height/2 * 2), 0);
             _av.point(i* 3,_av.height/2 + (_av.getAudioBuffer().get(i+1)* _av.height/2 * 2), 0);
-            //System.out.println("Co ord1 " + _av.height/2 + (temp[i]* _av.height/2) + "and Co ord 2 is " + _av.height/2+ (temp[i] *_av.height/2));
-          
-
+        
             _av.strokeWeight(2);
             _av.line(i* 3,_av.height/2 + (temp[i]* _av.height/2), 0, i* 3, _av.height/2+ (temp[i+1] *_av.height/2), 0);
-            //_av.point(i* 3,_av.height/2 + (temp[i]* _av.height/2), 0);
-           // _av.point(i* 3,_av.height/2 + (temp[i+1]* _av.height/2), 0);
-            // prevX= i * 3;
-            // prevY= _av.getAudioBuffer().get(i)* _av.getCy();
         }
-
-        //  //Amplitude 
-        //  float prevX=0;
-        //  float prevY=0;
-        //  for(int i = 0; i < _av.getAudioBuffer().size(); i++){
-        //      _av.stroke(PApplet.map(i, 0, _av.getAudioBuffer().size(), 0, 255), 255, 255);
-        //      _av.strokeWeight(1);
- 
-        //      _av.point(i* 3, _av.getCy() + (_av.getAudioBuffer().get(i)* _av.getCy()));
-        //      _av.line(i * 3 , _av.getCy() + (_av.getAudioBuffer().get(i)* _av.getCy()), prevX, prevY);
- 
- 
- 
-        //      _av.point(i* 3, _av.height/2 + (_av.getAudioBuffer().get(i)* _av.height/2));
- 
-        //      //float x;
-        //     // PApplet.map(i , 0,  _av.getAudioBuffer(),   0, width);  
-        //      //_av.line(i, _av.getCy() + (_av.getAudioBuffer().get(i)* _av.getCy()), i, _av.getCy() + (_av.getAudioBuffer().get(i) * _av.getCy()));
-        //      _av.line(i* 3,_av.height/2 + (_av.getAudioBuffer().get(i)* _av.height/2), i* 3, _av.height/2+ (_av.getAudioBuffer().get(i) *_av.height/2));
- 
- 
- 
- 
-        //      prevX= i * 3;
-        //      prevY= _av.getAudioBuffer().get(i)* _av.getCy();
-        //  }
-
-
-
-
-
-
-          // _av.noFill();
-
-            // _av.point(i* 3, _av.getCy() + (_av.getAudioBuffer().get(i)* _av.getCy()));
-            // _av.line(i * 3 , _av.getCy() + (_av.getAudioBuffer().get(i)* _av.getCy()), prevX, prevY);
-
-
-            // _av.point(i* 3, _av.height/2 + (_av.getAudioBuffer().get(i)* _av.height/2));
-            // _av.line(i * 3 , _av.getCy() + (_av.getAudioBuffer().get(i)* _av.getCy()), prevX, prevY);
-
-            // _av.point(i* 3, _av.height/2 + (temp[i]* _av.height/2));
-            // _av.line(i * 3 , _av.getCy() + (temp[i]* _av.getCy()), prevX, prevY);
-
-            //float x;
-           // PApplet.map(i , 0,  _av.getAudioBuffer(),   0, width);  
-            //_av.line(i, _av.getCy() + (_av.getAudioBuffer().get(i)* _av.getCy()), i, _av.getCy() + (_av.getAudioBuffer().get(i) * _av.getCy()));
-
-          
-            //_av.line(i* 3,_av.height/2 + (temp[i]* _av.height/2), i* 3, _av.height/2+ (temp[i] *_av.height/2));
-            //_av.line(i* 3,_av.height/2 + (_av.getAudioBuffer().get(i)* _av.height/2), i* 3, _av.height/2+ (_av.getAudioBuffer().get(i) *_av.height/2));
     } 
 }
--- java/src/C19409486/Cube.java
+++ java/src/C19409486/Cube.java
@@ -58,6 +58,39 @@ public class Cube extends MusicObject {
         _av.popMatrix();
         _angle += 0.01f;
     }
+
+    // public void moveCube(){
+    //     //
+    //     // _av.pushMatrix();
+    //     // _av.camera(0, 0, 0, 0, 0, -1, 0, 1, 0);
+    //     // _av.translate(0, 0, -200);
+    //     // _av.rotateX(_angle);
+    //     // _av.rotateZ(_angle);       
+    //     // float boxSize = 50 + (200 *  _av.getSmoothedAmplitude()); 
+    //     // _av.box(boxSize);   
+    //     // _av.popMatrix();
+    //     // _angle += 0.01f;
+
+
+    //     _av.calculateAverageAmplitude();
+    //     _av.stroke(PApplet.map( _av.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+    //     _av.strokeWeight(5);
+
+    //     _av.pushMatrix();
+    //     _av.camera(0, 0, 0, _av.height, _av.width, -1, 0, 1, 0);
+    //     //_av.size(200, 200, _av.P3D);
+    //     _av.translate(200, 200, 0); 
+    //     _av.rotateX(_angle);
+    //     _av.rotateZ(_angle);  
+    //     //_av.fill(PApplet.map( _av.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+    //      float boxSize = 50 + (100 *  _av.getSmoothedAmplitude()); 
+    //     _av.box(boxSize);   
+    //     _av.noFill();
+    //     _av.lights();
+    //     //_av.box(40);
+    //     _av.popMatrix();
+    //     _angle += 0.01f;
+    // }
 }
 
      
